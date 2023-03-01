c81a89e30b0b5dd7e1b3407ae740535139dae230
Salvador Espinosa <espinosalvador94@outlook.com>
2023-02-28 18:10:11 -0600

    added questions to JS

---
+++ assets/js/index.js
@@ -0,0 +1,102 @@
+const quizQuestions = [
+  {
+    question:
+      "Which method is used to get the value of an input fiel in JavaScript?",
+    answers: [
+      { Text: "getValue()", correct: false },
+      { Text: "getElementValue()", correct: false },
+      { Text: "document.inputValue()", correct: false },
+      { Text: "document.getElementbyId().value", correct: true },
+    ],
+  },
+  {
+    question:
+      "Which event occurs when the user clicks on an HTML element in JavaScript?",
+    answers: [
+      { Text: "onmouseover", correct: false },
+      { Text: "onmouseout", correct: false },
+      { Text: "onclick", correct: true },
+      { Text: "onsubmit", correct: false },
+    ],
+  },
+  {
+    question:
+      "Which property is used to change the CSS style of an element in JavaScript?",
+    answers: [
+      { Text: "cssStyle", correct: false },
+      { Text: "style", correct: true },
+      { Text: "setStyle", correct: false },
+      { Text: "setCss", correct: false },
+    ],
+  },
+  {
+    question:
+      "Which methos is used to add a new element to an HTML document in JavaScript?",
+    answers: [
+      { Text: "createNode()", correct: false },
+      { Text: "createElement()", correct: true },
+      { Text: "addElement()", correct: false },
+      { Text: "insertElement()", correct: false },
+    ],
+  },
+  {
+    question:
+      "Which method is used to remove an element from an HTML document in JavaScript?",
+    answers: [
+      { Text: "removeElement()", correct: false },
+      { Text: "deleteNode()", correct: false },
+      { Text: "removeNode()", correct: false },
+      { Text: "removeChild()", correct: true },
+    ],
+  },
+  {
+    question:
+      "Which method is used to change the text of an HTML element in JavaScript?",
+    answers: [
+      { Text: "setText()", correct: false },
+      { Text: "changeText()", correct: false },
+      { Text: "innerText()", correct: false },
+      { Text: "innerHTML()", correct: true },
+    ],
+  },
+  {
+    question:
+      "Which event occurse when an HTML form is submitted in JavaScript?",
+    answers: [
+      { Text: "onsubmit", correct: true },
+      { Text: "onformsubmit", correct: false },
+      { Text: "onclick", correct: false },
+      { Text: "onsubmitform", correct: false },
+    ],
+  },
+  {
+    question:
+      "which property is used to get the width of an HTML element in JavaScript?",
+    answers: [
+      { Text: "offsetWidth", correct: true },
+      { Text: "clientWidth", correct: false },
+      { Text: "width", correct: false },
+      { Text: "scrollWidth", correct: false },
+    ],
+  },
+  {
+    question:
+      "Which method is used to change the href attribute of an anchor element in JavaScript?",
+    answers: [
+      { Text: "setHref()", correct: false },
+      { Text: "changeHref()", correct: false },
+      { Text: "href()", correct: false },
+      { Text: "setAttribute", correct: true },
+    ],
+  },
+  {
+    question:
+      "Which event occurs when an HTML page has finished loading in JavaScript?",
+    answers: [
+      { Text: "onload", correct: true },
+      { Text: "onready", correct: false },
+      { Text: "onloaded", correct: false },
+      { Text: "onpageloaded", correct: false },
+    ],
+  },
+];
