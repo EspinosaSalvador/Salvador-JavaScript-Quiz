e6f9cb4924c7a810137b6b4f92c436316d2fba4c
Salvador Espinosa <espinosalvador94@outlook.com>
2023-03-02 20:02:14 -0600

    added readme text and i am still fixing also finished js

---
+++ README.md
@@ -0,0 +1,117 @@
+# Salvador-JavaScript-Quiz
+
+## Table of Content
+
+* [Introduction](#introduction)
+* [Features](#features)
+* [Requirements](#requirements)
+* [Installation](#installation)
+* [Configuration](#configuration)
+* [What you should be able to see](#what-you-should-be-able-to-see)
+* [Troubleshooting](#troubleshooting)
+* [Next Phase Development](#next-phase-development)
+* [FAQ (Frecuently Asked Questions)](#faq-frecuentrly-asked-questions)
+* [Team](#team)
+
+## Introduction
+
+This is a JavaScript quiz app that presents the user with a set of questions and multiple choices for eah question. The user can select an answer for each question and the app checks whether the answer is correct or not. the user has a limited amount of time to complete the quiz, and their final score is based on the number of correct answers and the remaining time.
+
+## Features
+
+* Randomized questions: The app presents the user with a randomized set of questions each time they start the quiz.
+* Multiple choices: Each question has four possible answers and the user must select one of them.
+* Timer: The user has a limited amount of time to complete the quiz. If the time runs out, the quiz ends automatically.
+* Score tracking: The user's score is based on the number of correct answers and the remaining time.
+* High score tracking: The app saves the user's high score in the local storage.
+* Save user's score: The app allows the user to save their score and initials in the local storage.
+
+## Requirements
+
+the requirements to run this webpage are really low due that we are only using *HTML*, *CSS* and *JS*.
+
+* Vs code
+* GitHub
+* Internet
+* Download JS to its latest release.
+
+## Installation
+
+To install this project please proceed to my [GitHub Repository](https://github.com/EspinosaSalvador/Password-Generator--Salvador-Espinosa-Valdez)
+
+you should be able to see this,
+
+![Github Repository](./Assets/Img/Github%20Repository.png)
+
+Click on code as shown in the image
+
+![Click on Code](./Assets/Img/SSH.png)
+
+Copy the SSH key or the HTTPS key and open.
+
+* Terminal
+* Git bash
+* CMD
+
+either one of this will work. please proceed to the terminal an put in your Terminal,
+
+```
+git clone git@github.com:EspinosaSalvador/Password-Generator--Salvador-Espinosa-Valdez.git
+```
+
+and now you have a copy of the code.
+
+## Configuration
+
+If you want to do any configurations on this project please open a new branch you can do this by putting the following code in your terminal,
+
+```
+git checkout -b ＜new-branch＞
+```
+
+switch branches putting the following code on your terminal.
+
+```
+git checkout ＜branchname＞
+```
+
+If you want to show us your code and merge please open a pull request on [Github](https://github.com/EspinosaSalvador/Password-Generator--Salvador-Espinosa-Valdez/pulls).
+
+![Pull request](./Assets/Img/Pull%20request.png)
+
+## What you should be able to see
+
+These is what you should be able to see in the Webpage,
+![Webpage](./Assets/Img/Webpage%20working.png)
+
+Please proceed to click on Generate Password. an alert should pop up on your screen like this img
+
+![Pop-up](./Assets/Img/generate%20password.png)
+
+please continue with and click confirm or cancel
+
+* OK = True or Yes
+
+* Cancel = False or No
+
+this is the way the password should be seen the webpage.
+
+![Team-page](./Assets/Img/password-true.png)
+
+Please use the button copy password to add a copy to your clipboard.
+
+## Troubleshooting
+
+The Password Generator is in version 1.0 ,please let me know if there are anybug in the webpage by issuing it in [Github](https://github.com/EspinosaSalvador/Password-Generator--Salvador-Espinosa-Valdez/issues)
+
+## Next Phase Development
+
+This are will be updated depending on the feedback from the users.
+
+## FAQ (Frecuentrly Asked Questions)
+
+We will update this part when we have some frequently asked question.
+
+## Team
+
+* Salvador Espinosa Valdez
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,120 +1,148 @@
-var containerEl = document.getElementsByClassName("container");
-var startButtonEl = document.getElementById("start-button");
-var quizEl = document.getElementById("quiz");
-var resultsEl = document.getElementById("results");
-var scoreEl = document.getElementById("score");
-var initialsEl = document.getElementById("initials");
-var hideEl = document.getElementsByClassName("hide");
-var quizQuestionsEL = document.getElementById("questions");
-
-var scoreEl = 0;
-var timeleft = 60;
-var timer;
-const quizQuestions = [
-  {
-    question:
-      "Which method is used to get the value of an input fiel in JavaScript?",
-    answers: [
-      { Text: "getValue()", correct: false },
-      { Text: "getElementValue()", correct: false },
-      { Text: "document.inputValue()", correct: false },
-      { Text: "document.getElementById().value", correct: true },
-    ],
-  },
-  {
-    question:
-      "Which event occurs when the user clicks on an HTML element in JavaScript?",
-    answers: [
-      { Text: "onmouseover", correct: false },
-      { Text: "onmouseout", correct: false },
-      { Text: "onclick", correct: true },
-      { Text: "onsubmit", correct: false },
-    ],
-  },
-  {
-    question:
-      "Which property is used to change the CSS style of an element in JavaScript?",
-    answers: [
-      { Text: "cssStyle", correct: false },
-      { Text: "style", correct: true },
-      { Text: "setStyle", correct: false },
-      { Text: "setCss", correct: false },
-    ],
-  },
-  {
-    question:
-      "Which method is used to add a new element to an HTML document in JavaScript?",
-    answers: [
-      { Text: "createNode()", correct: false },
-      { Text: "createElement()", correct: true },
-      { Text: "addElement()", correct: false },
-      { Text: "insertElement()", correct: false },
-    ],
-  },
-  {
-    question:
-      "Which method is used to remove an element from an HTML document in JavaScript?",
-    answers: [
-      { Text: "removeElement()", correct: false },
-      { Text: "deleteNode()", correct: false },
-      { Text: "removeNode()", correct: false },
-      { Text: "removeChild()", correct: true },
-    ],
-  },
-  {
-    question:
-      "Which method is used to change the text of an HTML element in JavaScript?",
-    answers: [
-      { Text: "setText()", correct: false },
-      { Text: "changeText()", correct: false },
-      { Text: "innerText()", correct: false },
-      { Text: "innerHTML()", correct: true },
-    ],
-  },
-  {
-    question:
-      "Which event occurse when an HTML form is submitted in JavaScript?",
-    answers: [
-      { Text: "onsubmit", correct: true },
-      { Text: "onformsubmit", correct: false },
-      { Text: "onclick", correct: false },
-      { Text: "onsubmitform", correct: false },
-    ],
-  },
-  {
-    question:
-      "which property is used to get the width of an HTML element in JavaScript?",
-    answers: [
-      { Text: "offsetWidth", correct: true },
-      { Text: "clientWidth", correct: false },
-      { Text: "width", correct: false },
-      { Text: "scrollWidth", correct: false },
-    ],
-  },
-  {
-    question:
-      "Which method is used to change the href attribute of an anchor element in JavaScript?",
-    answers: [
-      { Text: "setHref()", correct: false },
-      { Text: "changeHref()", correct: false },
-      { Text: "href()", correct: false },
-      { Text: "setAttribute", correct: true },
-    ],
-  },
-  {
-    question:
-      "Which event occurs when an HTML page has finished loading in JavaScript?",
-    answers: [
-      { Text: "onload", correct: true },
-      { Text: "onready", correct: false },
-      { Text: "onloaded", correct: false },
-      { Text: "onpageloaded", correct: false },
-    ],
-  },
+class Question {
+  constructor(text, answers, correctAnswerIndex) {
+    this.text = text;
+    this.answers = answers;
+    this.correctAnswerIndex = correctAnswerIndex;
+  }
+  isCorrectAnswer(index) {
+    return index === this.correctAnswerIndex;
+  }
+}
+const questions = [
+  new Question(
+    "Which country is known for the programming language JavaScript?",
+    ["USA", "France", "Germany", "Spain"],
+    0
+  ),
+  new Question(
+    "What is the largest planet in our solar system?",
+    ["Mars", "Venus", "Jupiter", "Saturn"],
+    2
+  ),
+  new Question(
+    "What year did World War II end?",
+    ["1944", "1945", "1946", "1947"],
+    1
+  ),
+  new Question(
+    "What is the highest mountain in the world?",
+    ["Mount Everest", "Mount Kilimanjaro", "Mount Rushmore", "Mount Fuji"],
+    0
+  ),
+  new Question(
+    "What is the most populous country in the world?",
+    ["India", "Russia", "China", "Brazil"],
+    2
+  ),
 ];
+const quizContainer = document.getElementById("quiz-container");
+const startButton = document.getElementById("start-button");
+const questionContainer = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-buttons");
+const scoreContainer = document.getElementById("score-container");
+const scoreElement = document.getElementById("score");
+const scoreFormContainer = document.getElementById("score-form-container");
 
-startButtonEl.addEventListener("click", startQuiz);
-
+let shuffledQuestions, currentQuestionIndex, timerInterval;
+// Start the quiz
+startButton.addEventListener("click", startQuiz);
+// Start the quiz
 function startQuiz() {
-  startButtonEl.classList.add("hide");
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  scoreElement.textContent = 0;
+  quizContainer.style.display = "none";
+  scoreContainer.style.display = "none";
+  scoreFormContainer.style.display = "none";
+  questionContainer.style.display = "block";
+  startTimer();
+  showQuestion();
+}
+// Show the current question and answers
+function showQuestion() {
+  const currentQuestion = shuffledQuestions[currentQuestionIndex];
+  questionElement.textContent = currentQuestion.text;
+  answerButtonsElement.innerHTML = "";
+  currentQuestion.answers.forEach((answer, index) => {
+    const button = document.createElement("button");
+    button.textContent = answer;
+    button.classList.add("btn");
+    button.addEventListener("click", () => {
+      selectAnswer(index);
+    });
+    answerButtonsElement.appendChild(button);
+  });
+}
+// Select an answer and check if it is correct
+function selectAnswer(index) {
+  const currentQuestion = shuffledQuestions[currentQuestionIndex];
+  if (currentQuestion.isCorrectAnswer(index)) {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < shuffledQuestions.length) {
+      showQuestion();
+    } else {
+      endQuiz();
+    }
+  } else {
+    subtractTime();
+    answerButtonsElement.innerHTML = "";
+    questionElement.textContent = "Wrong answer - 10 sec";
+    setTimeout(() => {
+      showQuestion();
+    }, 1000);
+  }
+}
+
+// Start the timer
+function startTimer() {
+  let timeLeft = 60;
+  timerInterval = setInterval(() => {
+    timeLeft--;
+    if (timeLeft >= 0) {
+      updateTimer(timeLeft);
+    } else {
+      endQuiz();
+    }
+  }, 1000);
+}
+// Subtract 10 seconds from the timer
+function subtractTime() {
+  const timeLeft = parseInt(document.getElementById("timer").textContent);
+  if (timeLeft >= 10) {
+    updateTimer(timeLeft - 10);
+  } else {
+    endQuiz();
+  }
+}
+// Update the timer display
+function updateTimer(timeLeft) {
+  document.getElementById("timer").textContent = timeLeft;
+}
+// End the quiz
+function endQuiz() {
+  clearInterval(timerInterval);
+  quizContainer.style.display = "none";
+  questionContainer.style.display = "none";
+  scoreContainer.style.display = "block";
+  scoreFormContainer.style.display = "block";
+  scoreElement.textContent = document.getElementById("timer").textContent;
+
+  // Save score in local storage
+  const score = parseInt(scoreElement.textContent);
+  const highScore = localStorage.getItem("highScore") || 0;
+
+  if (score > highScore) {
+    localStorage.setItem("highScore", score);
+  }
+
+  // Get the input value of initials
+  const initials = document.getElementById("initials").value.trim();
+
+  // Save the score and initials in local storage
+  if (initials && score > 0) {
+    const scores = JSON.parse(localStorage.getItem("scores")) || [];
+    scores.push({ score, initials });
+    localStorage.setItem("scores", JSON.stringify(scores));
+  }
 }
